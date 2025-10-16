7350929191ba485c9c0bb5b03202cef1c3326753
rrodriguez2025 <rrodriguez2025@alu.uct.cl>
2025-10-16 11:19:33 -0300

    fix(menu): actualizar referencias y ajustes en menu_catalog.py

--- Menu_catalog.py
+++ Menu_catalog.py
@@ -20,7 +20,7 @@ def get_default_menus() -> List[IMenu]:
                 Ingrediente("Pepsi", "unid", 1),
             ],
             precio=1100,
-            icono_path="IMG/icono_cola_lata_64x64.png",
+            icono_path="IMG/icono_pepsi_lata_64x64.png",
         ),
         CrearMenu(
             "Completo",
@@ -31,7 +31,7 @@ def get_default_menus() -> List[IMenu]:
                 Ingrediente("Tomate", "unid", 1),
             ],
             precio=1800,
-            icono_path="IMG/icono_hotdog_sin_texto_64x64.png",
+            icono_path="IMG/icono_completo_64x64.png",
         ),
         CrearMenu(
             "Hamburguesa",
@@ -41,7 +41,7 @@ def get_default_menus() -> List[IMenu]:
                 Ingrediente("Churrasco de carne", "unid", 1),
             ],
             precio=3500,
-            icono_path="IMG/icono_hamburguesa_negra_64x64.png",
+            icono_path="IMG/icono_hamburguesa_64x64.png",
         ),
         CrearMenu(
             "Panqueques",
@@ -51,7 +51,7 @@ def get_default_menus() -> List[IMenu]:
                 Ingrediente("Azúcar flor", "unid", 1),
             ],
             precio=2000,
-            icono_path="IMG/icono_empanada_queso_64x64.png",
+            icono_path="IMG/icono_panqueques_64x64.png",
         ),
         CrearMenu(
             "Pollo frito",
@@ -61,7 +61,7 @@ def get_default_menus() -> List[IMenu]:
                 Ingrediente("Porción de aceite", "unid", 1),
             ],
             precio=2800,
-            icono_path="IMG/icono_chorrillana_64x64.png",
+            icono_path="IMG/icono_pollo_64x64.png",
         ),
         CrearMenu(
             "Ensalada mixta",
@@ -71,6 +71,6 @@ def get_default_menus() -> List[IMenu]:
                 Ingrediente("Zanahoria rallada", "unid", 1),
             ],
             precio=1500,
-            icono_path="IMG/icono_cola_64x64.png",
+            icono_path="IMG/icono_ensalada_64x64.png",
         ),
     ]
